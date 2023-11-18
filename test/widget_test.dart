d33dddf9bcd38820cf0dc24d51f0a95158e71def
Botshelo Ramela <ramelabotshelo12@gmail.com>
2023-11-18 14:44:39 +0200

    REFACTOR: App name renaming

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fuel4u/main.dart';
+import 'package:liftshare/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
