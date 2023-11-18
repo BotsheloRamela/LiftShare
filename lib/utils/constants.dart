a4789d0fa7927dfb661a14daac8598b4cb53d416
Botshelo Ramela <ramelabotshelo12@gmail.com>
2023-11-18 20:50:41 +0200

    REFACTOR: Edit AppColors

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const Color gradientColor1 = Color(0xFF00E6FF);
   static const Color gradientColor2 = Color(0xFF1201FF);
   static const Color highlightColor = Color(0xFF858A94);
-  static const Color textColor2 = Color(0xFFFFFFFF);
+  static const Color textColor = Color(0xFFFFFFFF);
   static const Color backgroundColor = Color(0xFF011019);
   static const Color buttonColor = Color(0xFF141C27);
   static const Color warningColor = Color(0xFFFF3D00);
