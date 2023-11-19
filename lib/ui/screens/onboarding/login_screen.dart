c90be2732972d638e637d3e09ab8993df053c222
Botshelo Ramela <ramelabotshelo12@gmail.com>
2023-11-19 16:41:13 +0200

    FIX(ui): Prevent keyboard overflow issue on LoginScreen

--- lib/ui/screens/onboarding/login_screen.dart
+++ lib/ui/screens/onboarding/login_screen.dart
@@ -31,6 +31,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
     return SafeArea(
       child: Scaffold(
+        // resizeToAvoidBottomInset: false,
         extendBodyBehindAppBar: true,
         appBar: AppBar(
           title: const Text(
@@ -50,138 +51,137 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
         body: DecoratedBox(
           decoration: appBackground(),
-          child: Stack(
-            children: [
-              // appBackground(),
-              Padding(
-                padding: const EdgeInsets.all(AppValues.screenPadding),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 50),
-                      Container(
-                        height: 60,
-                        width: 60,
-                        padding: const EdgeInsets.all(1),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(
+                AppValues.screenPadding, AppValues.screenPadding,
+                AppValues.screenPadding, 0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 50),
+                    Container(
+                      height: 60,
+                      width: 60,
+                      padding: const EdgeInsets.all(1),
+                      decoration: const BoxDecoration(
+                        gradient: AppColors.gradientBackground,
+                        borderRadius: BorderRadius.all(Radius.circular(AppValues.largeBorderRadius)),
+                      ),
+                      child: Container(
+                        margin: const EdgeInsets.all(1),
                         decoration: const BoxDecoration(
-                          gradient: AppColors.gradientBackground,
-                          borderRadius: BorderRadius.all(Radius.circular(AppValues.largeBorderRadius)),
+                          borderRadius: BorderRadius.all(Radius.circular(AppValues.largeBorderRadius - 3)),
+                          color: AppColors.buttonColor,
                         ),
-                        child: Container(
-                          margin: const EdgeInsets.all(1),
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(AppValues.largeBorderRadius - 3)),
-                            color: AppColors.buttonColor,
-                          ),
-                          alignment: Alignment.center,
-                          child: SvgPicture.asset(
-                            'assets/icons/mail_icon.svg',
-                            height: 30,
-                            width: 30,
-                          ),
+                        alignment: Alignment.center,
+                        child: SvgPicture.asset(
+                          'assets/icons/mail_icon.svg',
+                          height: 30,
+                          width: 30,
                         ),
                       ),
-                      const SizedBox(height: 80),
-                      Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              _emailTextField(emailController),
+                    ),
+                    const SizedBox(height: 80),
+                    Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            _emailTextField(emailController),
 
-                              const SizedBox(height: 15),
+                            const SizedBox(height: 15),
 
-                              TextFormField(
-                                obscureText: _isPasswordVisible,
-                                controller: passwordController,
-                                decoration: InputDecoration(
-                                  labelText: 'Password',
-                                  filled: true,
-                                  fillColor: AppColors.buttonColor,
-                                  labelStyle: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.normal,
-                                    decoration: TextDecoration.none,
-                                    fontFamily: 'Aeonik',
-                                  ),
-
-                                  suffixIcon: IconButton(
-                                    icon: Icon(
-                                      _isPasswordVisible ? Icons.visibility : Icons
-                                          .visibility_off,
-                                      color: Colors.white,
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        _isPasswordVisible = !_isPasswordVisible;
-                                      });
-                                    },
-                                  ),
-
-                                  enabledBorder: const OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: AppColors.highlightColor,
-                                        width: 1.5,
-                                      ),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(
-                                              AppValues.largeBorderRadius)
-                                      )
-                                  ),
-                                  focusedBorder: const OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Colors.white,
-                                        width: 1.5,
-                                      ),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(
-                                              AppValues.largeBorderRadius)
-                                      )
-                                  ),
-                                ),
-                                style: const TextStyle(
+                            TextFormField(
+                              obscureText: _isPasswordVisible,
+                              controller: passwordController,
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                filled: true,
+                                fillColor: AppColors.buttonColor,
+                                labelStyle: const TextStyle(
                                   color: Colors.white,
                                   fontSize: 16,
                                   fontWeight: FontWeight.normal,
                                   decoration: TextDecoration.none,
                                   fontFamily: 'Aeonik',
                                 ),
-                              )
-                            ],
-                          )
-                      ),
-                      const SizedBox(height: 12),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: GestureDetector(
-                          onTap: () {
-                            // TODO: Navigate to Forgot Password Screen
-                          },
-                          child: const Text(
-                            'Forgot Password?',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              decoration: TextDecoration.none,
-                              fontWeight: FontWeight.normal,
-                              fontFamily: 'Aeonik',
-                            ),
+
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _isPasswordVisible ? Icons.visibility : Icons
+                                        .visibility_off,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isPasswordVisible = !_isPasswordVisible;
+                                    });
+                                  },
+                                ),
+
+                                enabledBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: AppColors.highlightColor,
+                                      width: 1.5,
+                                    ),
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(
+                                            AppValues.largeBorderRadius)
+                                    )
+                                ),
+                                focusedBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                      width: 1.5,
+                                    ),
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(
+                                            AppValues.largeBorderRadius)
+                                    )
+                                ),
+                              ),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                                decoration: TextDecoration.none,
+                                fontFamily: 'Aeonik',
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                    const SizedBox(height: 12),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: GestureDetector(
+                        onTap: () {
+                          // TODO: Navigate to Forgot Password Screen
+                        },
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            decoration: TextDecoration.none,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: 'Aeonik',
                           ),
                         ),
                       ),
-                      const SizedBox(height: 12),
-                      const Divider(
-                        thickness: 1,
-                        color: AppColors.highlightColor,
-                      ),
-                      const SizedBox(height: 12),
-                      _logInButton(context),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 12),
+                    const Divider(
+                      thickness: 1,
+                      color: AppColors.highlightColor,
+                    ),
+                    const SizedBox(height: 12),
+                    _logInButton(context),
+                  ],
                 ),
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),
